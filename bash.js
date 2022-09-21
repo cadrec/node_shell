c880d0d8c30079c02394eb020e24655c7c9a9d33
cadrec <ccarriga@chapman.edu>
2022-09-21 16:50:10 -0400

    Ls added

--- bash.js
+++ bash.js
@@ -1,11 +1,15 @@
 //output
+const ls = require('./ls');
+const pwd = require('./pwd');
 process.stdout.write('prompt > ');
 
 //response
 process.stdin.on('data', (data) => {
     if(data.toString().trim() === 'pwd'){
-        console.log(process.cwd());
-        process.stdout.write('\nprompt >');
+        pwd();
+    }
+    if(data.toString().trim() === 'ls'){
+        ls();
     }
     else{
     const cmd = data.toString().trim();//remove line
